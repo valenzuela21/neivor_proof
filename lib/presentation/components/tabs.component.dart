e8d731cd1914e5728b5b52f988036d0bca20fedd
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-16 08:39:36 -0500

    Neivor

---
+++ lib/presentation/components/tabs.component.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+class TabsComponent extends StatefulWidget {
+  const TabsComponent({Key? key}) : super(key: key);
+
+  @override
+  State<TabsComponent> createState() => _TabsComponentState();
+}
+
+class _TabsComponentState extends State<TabsComponent> {
+
+  bool stateTab = true;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      margin:  const EdgeInsets.only( bottom: 20),
+      child: Stack(
+          alignment: Alignment.center,
+          children:[
+            Container(
+              width: 320,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(50)
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      stateTab = true;
+                    });
+                  },
+                  child: Container(
+                    width: 160,
+                    decoration: BoxDecoration(
+                        color: stateTab ? Colors.white : Colors.transparent,
+                        borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      color: stateTab
+                          ? Colors.grey.withOpacity(0.2)
+                          : Colors.grey.withOpacity(0.0),
+                      spreadRadius: 4,
+                      blurRadius: 6,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text(
+                      "Contactenos",
+                      style: Theme.of(context).textTheme.titleSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      stateTab = false;
+                    });
+                  },
+                  child: Container(
+                    width: 160,
+                    decoration: BoxDecoration(
+                        color: !stateTab ? Colors.white : Colors.transparent,
+                        borderRadius: BorderRadius.circular(50),
+                      boxShadow: [
+                        BoxShadow(
+                          color: !stateTab
+                              ? Colors.grey.withOpacity(0.2)
+                              : Colors.grey.withOpacity(0.0),
+                          spreadRadius: 4,
+                          blurRadius: 6,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text("Favoritos",
+                        style: Theme.of(context).textTheme.titleSmall,
+                        textAlign: TextAlign.center),
+                  ),
+                )
+              ],
+            ),
+          ]),
+    );
+  }
+}
--- lib/presentation/pages/list_access.page.dart
+++ lib/presentation/pages/list_access.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:neivor/presentation/components/tabs.component.dart';
 import 'package:neivor/presentation/shared/layout/general.layout.dart';
 
 import '../ui/form/input.decoration.ui.dart';
@@ -9,92 +10,106 @@ class ListAccessPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size =  MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return GeneralLayout(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text('Autorizo Acceso', style:  GoogleFonts.aBeeZee(textStyle: Theme.of(context).textTheme.headlineMedium)),
-                const SizedBox(height: 20),
-                TextFormField(
-                  enableSuggestions: false,
-                  maxLength: 52,
-                  cursorColor: Colors.grey,
-                  autocorrect: false,
-                  style: const TextStyle(color: Colors.grey,  fontWeight: FontWeight.w400),
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecorations.generalInputDecoration(
-                      hinText: 'Buscar Contacto',
-                      labelText: 'Buscar...',
-                      prefixIcon: Icons.search,
-                      colorInput: Colors.grey,
-                      borderRadius: BorderRadius.circular(50)),
-                  validator: (String? value) {
-                    if (value == null || value.length < 4) {
-                      return "Ingresa el término de busqueda";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 10),
-                Text("Seleccione un contacto",
-                    style:  GoogleFonts.lato(textStyle: Theme.of(context).textTheme.displaySmall)),
-                const SizedBox(height: 20),
-                SizedBox(
-                  height: size.height / 2.5,
-                  child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return GestureDetector(onTap:
-                            (){
-                          Navigator.pushNamed(context, '/detail');
-                            },
-                          child: ListTile(
-                            leading:Icon(Icons.account_circle, size: 40,),
-                            title: Text("Alicia Linda", style: Theme.of(context).textTheme.titleSmall),
-                            subtitle: Text("(124) 145-7873"),
-                            trailing:Icon(Icons.arrow_forward_ios_rounded, size: 14,),
-                          ),
-                        );
-                      }),
-                ),
-                const SizedBox(height: 10),
-                MaterialButton(
-                    minWidth: double.infinity,
-                    shape: RoundedRectangleBorder(
-                        borderRadius:
-                        BorderRadius.circular(22.0)),
-                    disabledColor: Theme.of(context)
-                        .colorScheme
-                        .tertiary,
-                    focusColor: Theme.of(context)
-                        .colorScheme
-                        .tertiary,
-                    splashColor: Theme.of(context)
-                        .colorScheme
-                        .tertiary,
-                    highlightColor: Theme.of(context)
-                        .colorScheme
-                        .tertiary,
-                    elevation: 0,
-                    color:
-                    Theme.of(context).colorScheme.primary,
-                    onPressed: () => Navigator.of(context).pushNamed("/list_access"),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15),
-                      child: Text("Registrar Visitante".toUpperCase(),
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700)),
-                    )),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TabsComponent(),
+            Text('Autorizo Acceso',
+                style: GoogleFonts.aBeeZee(
+                    textStyle: Theme.of(context).textTheme.headlineMedium)),
+            const SizedBox(height: 20),
+            TextFormField(
+              enableSuggestions: false,
+              maxLength: 52,
+              cursorColor: Colors.grey,
+              autocorrect: false,
+              style: const TextStyle(
+                  color: Colors.grey, fontWeight: FontWeight.w400),
+              keyboardType: TextInputType.text,
+              decoration: InputDecorations.generalInputDecoration(
+                  hinText: 'Buscar Contacto',
+                  labelText: 'Buscar...',
+                  prefixIcon: Icons.search,
+                  colorInput: Colors.grey,
+                  borderRadius: BorderRadius.circular(50)),
+              validator: (String? value) {
+                if (value == null || value.length < 4) {
+                  return "Ingresa el término de busqueda";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            Text("Seleccione un contacto",
+                style: GoogleFonts.lato(
+                    textStyle: Theme.of(context).textTheme.displaySmall)),
+            const SizedBox(height: 20),
+            _ListaAccessContact(size: size),
+            const SizedBox(height: 10),
+            MaterialButton(
+                minWidth: double.infinity,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(22.0)),
+                disabledColor: Theme.of(context).colorScheme.tertiary,
+                focusColor: Theme.of(context).colorScheme.tertiary,
+                splashColor: Theme.of(context).colorScheme.tertiary,
+                highlightColor: Theme.of(context).colorScheme.tertiary,
+                elevation: 0,
+                color: Theme.of(context).colorScheme.primary,
+                onPressed: () =>
+                    Navigator.of(context).pushNamed("/list_access"),
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text("Registrar Visitante".toUpperCase(),
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w700)),
+                )),
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class _ListaAccessContact extends StatelessWidget {
+  const _ListaAccessContact({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size.height / 2.5,
+      child: ListView.builder(itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, '/detail');
+          },
+          child: ListTile(
+            leading: const Icon(
+              Icons.account_circle,
+              size: 40,
+            ),
+            title: Text("Alicia Linda",
+                style: Theme.of(context).textTheme.titleSmall),
+            subtitle: Text("(124) 145-7873"),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 14,
             ),
           ),
-        )
+        );
+      }),
     );
   }
 }
