cfc9b1b00f62e083ecdf5d2843c6c86e51923a25
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-17 00:38:22 -0500

    Update Animation

--- lib/constant/contanst.dart
+++ lib/constant/contanst.dart
@@ -1,3 +1,4 @@
+//Url Server Json: https://my-json-server.typicode.com/valenzuela21/server_json
 class NetworkApp {
   static const String _Host = 'my-json-server.typicode.com';
   static const String apiHost = '$_Host/valenzuela21';
--- lib/presentation/pages/access.page.dart
+++ lib/presentation/pages/access.page.dart
@@ -47,112 +47,119 @@ class AccessPage extends StatelessWidget {
     return GeneralLayout(
         child: Column(children: [
       Expanded(
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Container(
-              height: 250,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade300,
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20))),
-            ),
-            FadeIn(
-              duration: const Duration(milliseconds: 1000),
-              child: Positioned.fill(
-                  top: -30,
-                  child: Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        width: 330,
-                        height: 480,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white),
-                        child: Column(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(bottom: 10),
-                              height: 30,
-                              decoration: BoxDecoration(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  borderRadius: const BorderRadius.only(
-                                      topLeft: Radius.circular(20),
-                                      topRight: Radius.circular(20))),
-                            ),
-                            Image.asset(
-                              'assets/images/neivor.png',
-                              width: 100,
-                            ),
-                            const SizedBox(height: 10),
-                            const Text("Alicia Gonzales",
-                                style: TextStyle(
-                                    color: Colors.black87,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 24)),
-                            const SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Icon(Icons.business,
-                                    color:
-                                        Theme.of(context).colorScheme.secondary),
-                                const SizedBox(width: 5),
-                                Text("Torre 101",
-                                    style: Theme.of(context).textTheme.bodyLarge),
-                                const SizedBox(width: 10),
-                                Icon(Icons.account_circle,
-                                    color:
-                                        Theme.of(context).colorScheme.secondary),
-                                const SizedBox(width: 5),
-                                Text("Manuel Sanchez",
-                                    style: Theme.of(context).textTheme.bodyLarge),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "Francisco Javier Clavijero 51, CDMX, México",
-                              style: Theme.of(context).textTheme.bodyLarge,
-                              textAlign: TextAlign.center,
-                            ),
-                            QrImage(
-                              data: "1234567890",
-                              version: QrVersions.auto,
-                              size: 180.0,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "Entrada: Jueves 07 de Junio 2022 ",
-                              style: Theme.of(context).textTheme.bodyLarge,
-                              textAlign: TextAlign.center,
-                            ),
-                            Text(
-                              "Expira: 21/04/2023 - 18:30",
-                              style: Theme.of(context).textTheme.bodyLarge,
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "¿Quieres saber mas sobre neivor?",
-                              style: Theme.of(context).textTheme.bodyLarge,
-                              textAlign: TextAlign.center,
-                            ),
-                            TextButton(
-                              onPressed: _launchUrl,
-                              child: Text(
-                                "www.neivor.com",
+        child: FadeInDown(
+          duration: const Duration(milliseconds: 1000),
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Container(
+                height: 250,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+              ),
+               Positioned.fill(
+                    top: -30,
+                    child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          width: 330,
+                          height: 480,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                height: 30,
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).colorScheme.primary,
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                        topRight: Radius.circular(20))),
+                              ),
+                              Image.asset(
+                                'assets/images/neivor.png',
+                                width: 100,
+                              ),
+                              const SizedBox(height: 10),
+                              const Text("Alicia Gonzales",
+                                  style: TextStyle(
+                                      color: Colors.black87,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 24)),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.business,
+                                      color:
+                                          Theme.of(context).colorScheme.secondary),
+                                  const SizedBox(width: 5),
+                                  Text("Torre 101",
+                                      style: Theme.of(context).textTheme.bodyLarge),
+                                  const SizedBox(width: 10),
+                                  Icon(Icons.account_circle,
+                                      color:
+                                          Theme.of(context).colorScheme.secondary),
+                                  const SizedBox(width: 5),
+                                  Text("Manuel Sanchez",
+                                      style: Theme.of(context).textTheme.bodyLarge),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                "Francisco Javier Clavijero 51, CDMX, México",
                                 style: Theme.of(context).textTheme.bodyLarge,
                                 textAlign: TextAlign.center,
                               ),
-                            ),
-                          ],
-                        ),
-                      ))),
-            ),
-          ],
+                              QrImage(
+                                data: "1234567890",
+                                version: QrVersions.auto,
+                                size: 180.0,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                "Entrada: Jueves 07 de Junio 2022 ",
+                                style: Theme.of(context).textTheme.bodyLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.access_time_sharp, color: Colors.black45,),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    "Expira: 21/04/2023 - 18:30",
+                                    style: Theme.of(context).textTheme.bodyLarge,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                "¿Quieres saber mas sobre neivor?",
+                                style: Theme.of(context).textTheme.bodyLarge,
+                                textAlign: TextAlign.center,
+                              ),
+                              TextButton(
+                                onPressed: _launchUrl,
+                                child: Text(
+                                  "www.neivor.com",
+                                  style: Theme.of(context).textTheme.bodyLarge,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ))),
+            ],
+          ),
         ),
       ),
       FadeInUp(
